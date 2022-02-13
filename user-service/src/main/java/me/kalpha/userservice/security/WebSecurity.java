dae00c8c5bfd15a433bcf889233931f7b419f5b7
kalphageek <kalphageek@outlook.com>
2022-02-13 10:17:16 +0900

    User Microservice-테스트(jjd.deogi)

--- user-service/src/main/java/me/kalpha/userservice/security/WebSecurity.java
+++ user-service/src/main/java/me/kalpha/userservice/security/WebSecurity.java
@@ -48,7 +48,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("192.168.0.2")
+                .hasIpAddress("192.168.0.36")
                 .and()
                 .addFilter(getAuthenticationFilter()); // 모든 요청에 대해 필터작업 요청
 
